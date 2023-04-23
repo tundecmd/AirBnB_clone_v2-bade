0edadb5e022a4dd96224c8f9077da465bafcbdfb
Adepoju Lateef <badepojulateef@gmail.com>
2023-04-23 15:10:48 +0100

    added additional line space documentation task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,17 +10,20 @@ from flask import Flask
 """ Create a new Flask web application instance """
 app = Flask(__name__)
 
+
 """ Defines a route for the URL of the application """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ A view functions that returns a greeting message """
     return ("Hello HBNB!")
 
-""" Defines a route for the URL of the  """
+
+""" Defines a route for the URL of the """
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ A view function that returns the text 'HBNB' """
     return ("HBNB")
+
 
 """ Run the application if this script is executed directly """
 if __name__ == '__main__':

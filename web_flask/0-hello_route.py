638e77c584a8fe572d0c9aa565884bb6fb9e9ffe
Adepoju Lateef <badepojulateef@gmail.com>
2023-04-23 12:49:05 +0100

    changed the comment documentation in task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,16 +7,16 @@ Routes:
 """
 from flask import Flask
 
-# Create a new Flask web application instance
+""" Create a new Flask web application instance """
 app = Flask(__name__)
 
-# Defines a route for the URL of the application
+""" Defines a route for the URL of the application """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ A view functions that returns a greeting message """
     return "Hello HBNB!"
 
 
-# Run the application if this script is executed directly
+""" Run the application if this script is executed directly """
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
